fe6811efb7e5c34bf325d61f0f6756ee711d6659
psppatrick <119426@student.eastkent.ac.uk>
2019-01-24 15:05:53 +0000

    I am a massive idiot
    I put stuff in the wrong place :/

--- ARMBuildScripts/ARM64CrossCompilation.bash
+++ ARMBuildScripts/ARM64CrossCompilation.bash
@@ -26,12 +26,16 @@ cd ~
 mkdir -p arm-stuff/kernal/toolchains
 
 cd arm-stuff/kernal/toolchains
+#This is for arm architecture, I want ARM64
+#git clone git://github.com/offensive-security/gcc-arm-eabi-linaro-4.6.2.git
 
-git clone git://github.com/offensive-security/gcc-arm-eabi-linaro-4.6.2.git
+wget https://releases.linaro.org/archive/14.09/components/toolchain/binaries/gcc-linaro-aarch64-linux-gnu-4.9-2014.09_linux.tar.xz
+
+tar xvf gcc-linaro-aarch64-linux-gnu-4.9-2014.09_linux.tar.xz
 
 sleep 5s
 
-export ARCH=arm
-export CROSS_COMPILE=~/arm-stuff/kernel/toolchains/gcc-arm-eabi-linaro-4.6.2/bin/arm-eabi-
+export ARCH=arm64
+export CROSS_COMPILE=~/arm-stuff/kernel/toolchains/gcc-linaro-aarch64-linux-gnu-4.9-2014.09_linux
 
 echo "Done!" $?
